38c7314ca9fac2506fffb08e2211801e7807a1b6
XrosHeart23 <103415844+XrosHeart23@users.noreply.github.com>
2023-04-10 01:00:57 +0800

    Update formatting

--- src/services/npmService.ts
+++ src/services/npmService.ts
@@ -3,11 +3,7 @@ import axios from "axios";
 // const NPM_REGISTRY_URL = "https://registry.npmjs.org";
 const NPM_SEARCH_URL = "https://registry.npmjs.com/-/v1/search";
 
-export const searchPackages = async (
-    searchTerm: string,
-    resultsPerPage: number,
-    offset: number,
-) => {
+export const searchPackages = async (searchTerm: string, resultsPerPage: number, offset: number) => {
     try {
         const response = await axios.get(NPM_SEARCH_URL, {
             params: {
